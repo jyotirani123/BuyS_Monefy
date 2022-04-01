1acd014ca220c3cd655dac80c4610695414e3e66
Tania <taniachanana02@gmail.com+Tania@users.noreply.github.com>
2022-04-01 19:31:40 +0530

    styling is done

--- client_fe/src/components/home/Home.js
+++ client_fe/src/components/home/Home.js
@@ -3,40 +3,275 @@ import './Home.css';
 import Header from '../header/Header';
 import bgimage from '../../assets/im.png'
 import aboutimage from '../../assets/about.png';
+import contactimage from '../../assets/contact.png';
+import logo from '../../assets/logo.png';
 function Home() {
     return (
         <>
-    <div className="home">
-        <Header />
-    {/* <h1>Buys Monefy</h1> */}
-    <div className='d-flex align-items-center justify-content-between container pt-4'>
-        <div className='home-details'>
-            {/* <h1 className="home-details-heading">Welcome to <br /><span>BuyS Monefy</span></h1>
+            <div className="home">
+                <Header />
+                {/* <h1>Buys Monefy</h1> */}
+                <div className='d-flex align-items-center justify-content-between container pt-4 home-container'>
+                    <div className='home-details'>
+                        {/* <h1 className="home-details-heading">Welcome to <br /><span>BuyS Monefy</span></h1>
             <h4>A website to settle buyer and supplier payment.</h4> */}
-            <h1 class="display-3 text-center text-md-start">
-              Welcome to <br /> <span class="text-primary">BuyS Monefy</span>.
-            </h1>
-            <h4 class="display-6 text-center text-md-start">
-              Transactions made simple.
-              </h4>
-            <p class="lead text-center text-md-start text-muted mb-6 mb-lg-8">
-            A website to settle buyer and supplier payment.
-            </p>
-            <a href="overview.html" class="btn btn-lg btn-primary shadow lift me-1">
-                Login
-              </a>
-        </div>
-        <img src = {bgimage} />
-    </div>
-    <div className='about py-5 my-3 d-flex flex-column align-items-center'>
-        <h1 class="display-3 text-md-start text-primary my-4">About</h1>
-        <div className='d-flex justify-content-between align-items-center'>
-            <img src = {aboutimage} />
-            <p className='lead text-center text-md-start text-muted mb-6 mb-lg-8'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-        </div>
-    </div>
-    </div>
-    </>
+                        <h1 class="display-3 text-center text-md-start">
+                            Welcome to <br /> <span class="text-primary">BuyS Monefy</span>.
+                        </h1>
+                        <h4 class="display-6 text-center text-md-start">
+                            Transactions made simple.
+                        </h4>
+                        <p class="lead text-center text-md-start text-muted mb-6 mb-lg-8">
+                            A website to settle buyer and supplier payment.
+                        </p>
+                        <a href="overview.html" class="btn btn-lg btn-primary shadow lift me-1">
+                            Login
+                        </a>
+                    </div>
+                    <img src={bgimage} />
+                </div>
+                <div className='about py-5 my-3 d-flex flex-column align-items-center'>
+                    <h1 class="display-3 text-md-start text-primary my-4">About</h1>
+                    <div className='d-flex justify-content-between align-items-center'>
+                        <img src={aboutimage} />
+                        <p className='lead text-center text-md-start text-muted mb-6 mb-lg-8'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                    </div>
+                </div>
+
+                <div className='service py-5 my-3 d-flex flex-column align-items-center'>
+                    <h1 class="display-3 text-md-start text-primary my-4">Services</h1>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-md-4 aos-init aos-animate" data-aos="fade-up">
+                                <div class="icon text-primary mb-3">
+                                    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path d="M7 3h10a4 4 0 110 8H7a4 4 0 110-8zm0 6a2 2 0 100-4 2 2 0 000 4z" fill="#335EEA"></path><path d="M7 13h10a4 4 0 110 8H7a4 4 0 110-8zm10 6a2 2 0 100-4 2 2 0 000 4z" fill="#335EEA" opacity=".3"></path></g></svg>            </div>
+
+                                <h3>
+                                    Built for Buyers
+                                </h3>
+                                <p class="text-muted mb-6 mb-md-0">
+                                    Landkit is built to make your life easier. Variables, build tooling, documentation, and reusable components.
+                                </p>
+
+                            </div>
+                            <div class="col-12 col-md-4 aos-init aos-animate" data-aos="fade-up" data-aos-delay="50">
+
+                                <div class="icon text-primary mb-3">
+                                    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path d="M5.5 4h4A1.5 1.5 0 0111 5.5v1A1.5 1.5 0 019.5 8h-4A1.5 1.5 0 014 6.5v-1A1.5 1.5 0 015.5 4zm9 12h4a1.5 1.5 0 011.5 1.5v1a1.5 1.5 0 01-1.5 1.5h-4a1.5 1.5 0 01-1.5-1.5v-1a1.5 1.5 0 011.5-1.5z" fill="#335EEA"></path><path d="M5.5 10h4a1.5 1.5 0 011.5 1.5v7A1.5 1.5 0 019.5 20h-4A1.5 1.5 0 014 18.5v-7A1.5 1.5 0 015.5 10zm9-6h4A1.5 1.5 0 0120 5.5v7a1.5 1.5 0 01-1.5 1.5h-4a1.5 1.5 0 01-1.5-1.5v-7A1.5 1.5 0 0114.5 4z" fill="#335EEA" opacity=".3"></path></g></svg>            </div>
+
+                                <h3>
+                                    Built for Suppliers
+                                </h3>
+                                <p class="text-muted mb-6 mb-md-0">
+                                    Designed with the latest design trends in mind. Landkit feels modern, minimal, and beautiful.
+                                </p>
+
+                            </div>
+                            <div class="col-12 col-md-4 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+
+                                <div class="icon text-primary mb-3">
+                                    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path d="M17.272 8.685a1 1 0 111.456-1.37l4 4.25a1 1 0 010 1.37l-4 4.25a1 1 0 01-1.456-1.37l3.355-3.565-3.355-3.565zm-10.544 0L3.373 12.25l3.355 3.565a1 1 0 01-1.456 1.37l-4-4.25a1 1 0 010-1.37l4-4.25a1 1 0 011.456 1.37z" fill="#335EEA"></path><rect fill="#335EEA" opacity=".3" transform="rotate(15 12 12)" x="11" y="4" width="2" height="16" rx="1"></rect></g></svg>            </div>
+
+                                <h3>
+                                    Built for Banks
+                                </h3>
+                                <p class="text-muted mb-0">
+                                    We've written extensive documentation for components and tools, so you never have to reverse engineer anything.
+                                </p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='contact py-5 my-3 d-flex flex-column align-items-center'>
+                    <h1 class="display-3 text-md-start text-primary my-4">Contact Us</h1>
+                    <div className='d-flex justify-content-center align-items-center contact-container'>
+                        <img src={contactimage} />
+                        <form className='form-container'>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1" className='mb-2'>Email address</label>
+                                <input type="email" class="form-control mb-2" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1" className='mb-2'>Name</label>
+                                <input type="text" class="form-control mb-2" id="exampleInputName1" placeholder="Enter name" />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputMessage1" className='mb-2'>Message</label>
+                                <textarea rows={5} type="text" class="form-control mb-2" id="exampleInputMessage1" placeholder="Enter Message" />
+                            </div>
+
+                            <button type="submit" class="btn btn-primary ">Submit</button>
+                        </form>
+                    </div>
+                </div>
+                <footer class="py-8 py-md-11 bg-gray-200">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-md-4 col-lg-3">
+                                <img src={logo} alt="..." class="footer-brand img-fluid mb-2" />
+                                    <p class="text-gray-700 mb-2">
+                                        A better way to build.
+                                    </p>
+                                    <ul class="list-unstyled list-inline list-social mb-6 mb-md-0">
+                                        <li class="list-inline-item list-social-item me-3">
+                                            <a href="#!" class="text-decoration-none">
+                                                <img src="./assets/img/icons/social/instagram.svg" class="list-social-icon" alt="..." />
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item list-social-item me-3">
+                                            <a href="#!" class="text-decoration-none">
+                                                <img src="./assets/img/icons/social/facebook.svg" class="list-social-icon" alt="..." />
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item list-social-item me-3">
+                                            <a href="#!" class="text-decoration-none">
+                                                <img src="./assets/img/icons/social/twitter.svg" class="list-social-icon" alt="..." />
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item list-social-item">
+                                            <a href="#!" class="text-decoration-none">
+                                                <img src="./assets/img/icons/social/pinterest.svg" class="list-social-icon" alt="..." />
+                                            </a>
+                                        </li>
+                                    </ul>
+
+                            </div>
+                            <div class="col-6 col-md-4 col-lg-2">
+                                <h6 class="fw-bold text-uppercase text-gray-700">
+                                    Products
+                                </h6>
+                                <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Page Builder
+                                        </a>
+                                    </li>
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            UI Kit
+                                        </a>
+                                    </li>
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Styleguide
+                                        </a>
+                                    </li>
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Documentation
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-reset">
+                                            Changelog
+                                        </a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            <div class="col-6 col-md-4 col-lg-2">
+                                <h6 class="fw-bold text-uppercase text-gray-700">
+                                    Services
+                                </h6>
+                                <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Documentation
+                                        </a>
+                                    </li>
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Changelog
+                                        </a>
+                                    </li>
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Pagebuilder
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-reset">
+                                            UI Kit
+                                        </a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            <div class="col-6 col-md-4 offset-md-4 col-lg-2 offset-lg-0">
+                                <h6 class="fw-bold text-uppercase text-gray-700">
+                                    Connect
+                                </h6>
+                                <ul class="list-unstyled text-muted mb-0">
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Page Builder
+                                        </a>
+                                    </li>
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            UI Kit
+                                        </a>
+                                    </li>
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Styleguide
+                                        </a>
+                                    </li>
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Documentation
+                                        </a>
+                                    </li>
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Changelog
+                                        </a>
+                                    </li>
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Documentation
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-reset">
+                                            Changelog
+                                        </a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            <div class="col-6 col-md-4 col-lg-2">
+                                <h6 class="fw-bold text-uppercase text-gray-700">
+                                    Legal
+                                </h6>
+                                <ul class="list-unstyled text-muted mb-0">
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Documentation
+                                        </a>
+                                    </li>
+                                    <li class="mb-3">
+                                        <a href="#!" class="text-reset">
+                                            Changelog
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#!" class="text-reset">
+                                            Pagebuilder
+                                        </a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div> 
+                </footer>
+            </div>
+        </>
     )
 }
 
